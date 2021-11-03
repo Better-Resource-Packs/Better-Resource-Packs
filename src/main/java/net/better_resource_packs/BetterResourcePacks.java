d4865767e4c553de4a39bf1d7e0225ebfacaf763
Julien Constant <49886317+Juknum@users.noreply.github.com>
2021-11-03 18:50:56 +0100

    init

---
+++ src/main/java/net/better_resource_packs/BetterResourcePacks.java
@@ -0,0 +1,10 @@
+package net.better_resource_packs;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BetterResourcePacks implements ModInitializer {
+  @Override
+  public void onInitialize() {
+
+  }
+}
