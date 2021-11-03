2ee7dc64ff37bd668c095b1375ffac20dc0879a9
Julien Constant <49886317+Juknum@users.noreply.github.com>
2021-11-03 23:24:49 +0100

    modified mod id

---
+++ src/main/java/net/brp/BetterResourcePacks.java
@@ -0,0 +1,15 @@
+package net.brp;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BetterResourcePacks implements ModInitializer {
+  public static final Logger LOG = LogManager.getLogger("BRP");
+  public static final String MODID = "better_resource_packs";
+
+  @Override
+  public void onInitialize() {
+  }
+}
